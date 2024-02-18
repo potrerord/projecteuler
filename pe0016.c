9a49c753259a3cfdf71bcfe2a9bb425b9d9e28ee
potrerord <125526503+potrerord@users.noreply.github.com>
2024-02-18 09:53:54 -0500

    Created a C file for problem 16

---
+++ pe0016.c
@@ -0,0 +1,18 @@
+/**
+ * @file pe0016.c
+ * @author Anthony Narag
+ * @brief Project Euler Problem 16.
+ * @version 0
+ * @date 2024-02-18-sun
+ *
+ * Problem 16
+ * 2^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+ * What is the sum of the digits of the number 2^1000?
+ */
+
+#include <stdio.h>
+
+int main(void)
+{
+    
+}
