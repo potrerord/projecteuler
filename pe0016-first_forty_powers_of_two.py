9fd1cb31cf287b572c4b0c60a259f96d026fcbbc
potrerord <125526503+potrerord@users.noreply.github.com>
2024-02-21 07:56:17 -0500

    new progress on problem 16, began searching for pattern in final two digits of powers of 2

---
+++ pe0016-first_forty_powers_of_two.py
@@ -0,0 +1,8 @@
+"""
+2024-02-21-wed
+Anthony Narag
+Project Euler Problem 16
+pe0016-first_forty_powers_of_two.py
+
+The final digit of powers of 2 cycle through {2, 4, 8, 6} -- I'm trying to find repeatable behavior in the final two digits of power
+"""
